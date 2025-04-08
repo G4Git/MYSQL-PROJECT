5c65c94907f2c257f2c4a27e00eae9768285c5e3
GOBIND SINGH <pbx1gobind@gmail.com>
2025-04-08 14:34:27 +0530

    page layout done

--- Project/page1/index.js
+++ Project/page1/index.js
@@ -3,8 +3,15 @@
 const home = document.querySelector(".Cards")
 const commands = [
     {
-    name: "Select",
-    desc:"Select is a Dql Cmd"
+        name: "Select",
+        desc: "Select is a Dql Cmd",
+        code: "SELECT * FROM users WHERE age > 18;",
+        output:`
+           id	name	age
+            1	John	25
+            2	Doe	    30
+            3	Max	    17
+        `
     },
     {
         name: "Query",
@@ -28,12 +35,34 @@ const commands = [
         }
 ]
 
+
+
+
 commands.forEach((value) => {
     const container = document.createElement("div")
-    container.setAttribute("class", "container flex-col flex items-center rounded-md justify-center  bg-white sm:w-[100%] p-3  gap-2 w-[100%] sm:h-[80%] h-48 ")
+    container.setAttribute("class", "container flex-col flex items-center rounded-md justify-center  bg-white sm:w-[100%] p-3  gap-4 w-[100%] h-[100%]   ")
     home.appendChild(container)
-    container.innerHTML = `<h1 class="text-2xl font-semibold  ">${value.name}</h1>
+    container.innerHTML = `
+    <div class="flex-col justify-center items-center flex gap-1 py-2">
+    <h1 class="text-2xl font-semibold  ">${value.name}</h1>
     <p class="text-lg ">${value.desc}</p>
+    </div>
+
+    <div class="w-full h-full rounded-md  flex flex-col  justify-center bg-neutral-600 border-2 border-neutral-600 ">
+    <h1 class="px-3 p-1 text-yellow-400">Input</h1>
+    <code class="bg-black  w-[100%] overflow-x-auto  p-4 text-white" >
+    ${value.code}
+    </code>
+    </div>
+
+    <div class="w-full h-full rounded-md flex flex-col  justify-center bg-neutral-600 border-2 border-neutral-600 object-cover  ">
+    <h1 class="px-3 p-1 text-green-400">Output</h1>
+    <div class="h-32 w-full flex justify-center items-center  bg-black">
+    <textarea  class="bg-black w-full flex-col flex justify-center items-center   overflow-auto  h-full text-white focus:outline-none" >
+    ${value.output}   
+    </textarea>
+  </div>
+    </div>
     `
     
 })
